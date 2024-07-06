a3c7afafe68b0b9743d37ce6ae3f6f7e3f25ec47
MRAKCHI <163375217+Mrakchidev@users.noreply.github.com>
2024-07-06 11:37:59 +0100

    owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -19,7 +19,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/dYhpBJJVNe'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n 👨‍💻 Myself Hamza aka Mrakchi. I'm a Discord bot developer and a newbie to web development. I love playing games and creating different web server applications. Feel free to contact me!\n YouTube : ❤️ [Click here](${youtubeLink})\n Instagram : 💙 [Click here](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
